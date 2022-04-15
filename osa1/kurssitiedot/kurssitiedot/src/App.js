7c6519cc8c0650c4956ae22305fb860f90624c10
sofiaaair <sofia.airas@hotmail.com>
2022-04-15 22:04:57 +0300

    tehtava 1.5

--- osa1/kurssitiedot/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/kurssitiedot/src/App.js
@@ -39,8 +39,9 @@ const Part = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [ 
+  const course = {
+    name: 'Half Stack application development',
+    parts: [ 
     {
     name: 'Fundamentals of React',
     exercises: 10
@@ -54,12 +55,12 @@ const App = () => {
     exercises: 14
     }
   ]
-
+ }
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
   )
 }
