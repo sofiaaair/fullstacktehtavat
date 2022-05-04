e0d0d79a29b71af3a49bdc6ed656e383b47066b0
sofiaaair <sofia.airas@hotmail.com>
2022-05-04 21:46:59 +0300

    tehtava 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,22 +1,38 @@
 import { useState } from 'react'
 
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll]= useState(0)
+
+const handleGoodClick = () =>{
+  setGood(good+1)
+  setAll(all+1)
+}
+const handleNeutralClick = () =>{
+  setNeutral(neutral+1)
+  setAll(all+1)
+}
+const handleBadClick = () =>{
+  setBad(bad+1)
+  setAll(all+1)
+}
 
   return (
     <div>
       <div>
         <h1> Give feedback</h1>
-        <button onClick={() => setGood(good +1)}>
+        <button onClick={handleGoodClick}>
           good
         </button>
-        <button onClick={() => setNeutral(neutral +1)}>
+        <button onClick={handleNeutralClick}>
           neutral
         </button>
-        <button onClick={() => setBad(bad +1)}>
+        <button onClick={handleBadClick}>
           bad
         </button>
       </div>
@@ -25,6 +41,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {(good*1)+(bad*-1)/all}</p>
+        <p>positive {good/all} %</p>
       </div>
     </div>
   )
