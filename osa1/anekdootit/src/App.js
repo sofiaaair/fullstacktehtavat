5e8a3334650267418cdfb9c09f5c35ee1a263e8e
sofiaaair <sofia.airas@hotmail.com>
2023-02-02 14:10:55 +0200

    tehtavat 1.14 asti

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+const MaxAnecdote = ({anecdotes, votes}) => {
+
+  let max = Math.max(...votes)
+
+  const isNumber = (number) => number ===max
+  let index = votes.findIndex(isNumber)
+  return(
+    <div>
+      <p>{anecdotes[index]}</p>
+      <p>has {max} votes</p>
+    </div>
+  )
+
+}
+
 const App = ()=> {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -12,17 +27,35 @@ const App = ()=> {
     'The only way to go fast, is to go well.'
   ]
 
-  const selectRandomNumber = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
-  }
+  const selectRandomNumber = () => setSelected(Math.floor(Math.random() * anecdotes.length))
   
+
+  const voteAnecdote = () => {
+    const copy = [...vote]
+    copy[selected] += 1
+    setVote(copy)
+    
+  }
+
+  
+  const points = Array(anecdotes.length).fill(0)
   const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(points)
+
+
   return (
     <div>
+      <h1> Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
+      <p>has {vote[selected]} votes</p>
+      <button onClick={voteAnecdote}>
+        vote
+      </button>
       <button onClick={selectRandomNumber} >
         next anecdote
       </button>
+      <h1>Anecdote with most votes</h1>
+      <MaxAnecdote anecdotes= {anecdotes} votes = {vote}/>
     </div>
   );
 }
